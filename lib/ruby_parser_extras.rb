bd430d0642276c4009fa768bd8efcce1c1db4f65
Ryan Davis <ryand@zenspider.com>
2012-07-26 15:52:20 -0800

    prepped for release [git-p4: depot-paths = "//src/ruby_parser/dev/": change = 7671]

--- lib/ruby_parser_extras.rb
+++ lib/ruby_parser_extras.rb
@@ -78,7 +78,7 @@ class RPStringScanner < StringScanner
 end
 
 module RubyParserStuff
-  VERSION = '3.0.0.a3' unless constants.include? "VERSION" # SIGH
+  VERSION = '3.0.0.a4' unless constants.include? "VERSION" # SIGH
 
   attr_accessor :lexer, :in_def, :in_single, :file
   attr_reader :env, :comments
