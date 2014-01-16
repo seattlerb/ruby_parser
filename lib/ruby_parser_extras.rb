d4938d59d6f95194df32873a67298cc77487c7b8
Ryan Davis <ryand@zenspider.com>
2014-01-16 15:21:11 -0800

    + Removed RubyParserExtras#arg_add. (presidentbeef! YAY!) + Cleaned up and re-ordered node_assign to be faster (ordered by actual occurrance).
    [git-p4: depot-paths = "//src/ruby_parser/dev/": change = 9185]

--- lib/ruby_parser_extras.rb
+++ lib/ruby_parser_extras.rb
@@ -140,15 +140,6 @@ module RubyParserStuff
     raise RubyParser::SyntaxError, msg
   end
 
-  def arg_add(node1, node2) # TODO: nuke
-    return s(:arglist, node2) unless node1
-
-    node1[0] = :arglist if node1[0] == :array
-    return node1 << node2 if node1[0] == :arglist
-
-    return s(:arglist, node1, node2)
-  end
-
   def arg_blk_pass node1, node2 # TODO: nuke
     node1 = s(:arglist, node1) unless [:arglist, :call_args, :array, :args].include? node1.first
     node1 << node2 if node2
@@ -556,7 +547,7 @@ module RubyParserStuff
   end
 
   def argl x
-    x = s(:arglist, x) if x and x[0] != :arglist
+    x = s(:arglist, x) if x and x[0] == :array
     x
   end
 
@@ -580,12 +571,13 @@ module RubyParserStuff
     # TODO: need a test with f(&b) to produce block_pass
     # TODO: need a test with f(&b) { } to produce warning
 
-    args ||= s(:arglist)
-    args[0] = :arglist if [:args, :array, :call_args].include? args.first
-    args = s(:arglist, args) unless args.first == :arglist
-
-    # HACK quick hack to make this work quickly... easy to clean up above
-    result.concat args[1..-1]
+    if args
+      if [:arglist, :args, :array, :call_args].include? args.first
+        result.concat args.sexp_body
+      else
+        result << args
+      end
+    end
 
     line = result.grep(Sexp).map(&:line).compact.min
     result.line = line if line
@@ -927,18 +919,26 @@ module RubyParserStuff
     rhs = value_expr rhs
 
     case lhs[0]
-    when :gasgn, :iasgn, :lasgn, :masgn, :cdecl, :cvdecl, :cvasgn then
+    when :lasgn, :iasgn, :cdecl, :cvdecl, :gasgn, :cvasgn, :attrasgn then
       lhs << rhs
-    when :attrasgn then
-      lhs << rhs
-    when :call then
-      args = lhs.pop unless Symbol === lhs.last
-      lhs.concat arg_add(args, rhs)[1..-1]
     when :const then
       lhs[0] = :cdecl
       lhs << rhs
+    when :call then
+      # TODO: prove this can happen. I don't think it can and gauntlet/a agrees.
+      if Symbol === lhs.last
+        lhs << rhs
+      else
+        args = lhs.pop
+
+        if args.nil?
+          lhs << rhs
+        else
+          lhs << args << rhs
+        end
+      end
     else
-      raise "unknown lhs #{lhs.inspect}"
+      raise "unknown lhs #{lhs.inspect} w/ #{rhs.inspect}"
     end
 
     lhs
