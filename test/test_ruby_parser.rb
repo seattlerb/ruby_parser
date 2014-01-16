15a51badb8fb5d7437da2b2f21b1c361f4a576f2
Justin Collins <justin@presidentbeef.com>
2014-01-16 08:44:47 -0800

    Add test for line break after ivar argument

--- test/test_ruby_parser.rb
+++ test/test_ruby_parser.rb
@@ -677,6 +677,22 @@ module TestRubyParserShared
     assert_equal 2, result[3].line,   "call should have line number"
   end
 
+  def test_parse_line_call_ivar_line_break_paren
+    rb = "a(@b\n)"
+    pt = s(:call, nil, :a, s(:ivar, :@b))
+
+    assert_parse rb, pt
+    assert_equal 1, result[3].line
+  end
+
+  def test_parse_line_call_ivar_arg_no_parens_line_break
+    rb = "a @b\n"
+    pt = s(:call, nil, :a, s(:ivar, :@b))
+
+    assert_parse rb, pt
+    assert_equal 1, result[3].line
+  end
+
   def test_parse_line_defn_no_parens
     pt = s(:defn, :f, s(:args), s(:nil))
 
