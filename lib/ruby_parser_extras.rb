10d4daec504f8502629d57fc268b29bcb2126942
Ryan Davis <ryand@zenspider.com>
2018-12-04 13:25:02 -0800

    prepped for release [git-p4: depot-paths = "//src/ruby_parser/dev/": change = 11778]

--- lib/ruby_parser_extras.rb
+++ lib/ruby_parser_extras.rb
@@ -7,7 +7,7 @@ require "rp_extensions"
 require "rp_stringscanner"
 
 module RubyParserStuff
-  VERSION = "3.11.0"
+  VERSION = "3.12.0"
 
   attr_accessor :lexer, :in_def, :in_single, :file
   attr_reader :env, :comments
