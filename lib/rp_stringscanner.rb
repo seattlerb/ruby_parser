f3a59c500db7ceca18ad8d701befbb8bd05fe6c0
Ryan Davis <ryand-ruby@zenspider.com>
2021-10-06 01:11:43 -0800

    ! Overhauled RPStringScanner. Removed OLD compatibility methods!
    ! Removed string_to_pos, charpos, etc hacks for ancient ruby versions.
    ! Removed unread_many... NO! NO EDITING THE INPUT STRING!
    - Fixed caller for getch and scan when DEBUG=1
    + Added TALLY and DEBUG options for ss.getch and ss.scan
    
    [git-p4: depot-paths = "//src/ruby_parser/dev/": change = 13200]

--- lib/rp_stringscanner.rb
+++ lib/rp_stringscanner.rb
@@ -1,64 +1,35 @@
 require "strscan"
 
 class RPStringScanner < StringScanner
-#   if ENV['TALLY'] then
-#     alias :old_getch :getch
-#     def getch
-#       warn({:getch => caller[0]}.inspect)
-#       old_getch
-#     end
-#   end
-
-  if "".respond_to? :encoding then
-    if "".respond_to? :byteslice then
-      def string_to_pos
-        string.byteslice(0, pos)
-      end
-    else
-      def string_to_pos
-        string.bytes.first(pos).pack("c*").force_encoding(string.encoding)
-      end
-    end
-
-    def charpos
-      string_to_pos.length
-    end
-  else
-    alias :charpos :pos
-
-    def string_to_pos
-      string[0..pos]
-    end
-  end
-
-  def unread_many str # TODO: remove this entirely - we should not need it
-    warn({:unread_many => caller[0]}.inspect) if ENV['TALLY']
-    begin
-      string[charpos, 0] = str
-    rescue IndexError
-      # HACK -- this is a bandaid on a dirty rag on an open festering wound
-    end
-  end
-
-  if ENV['DEBUG'] then
+  if ENV["DEBUG"] || ENV["TALLY"] then
     alias :old_getch :getch
     def getch
       c = self.old_getch
-      p :getch => [c, caller.first]
+      where = caller.drop_while { |s| s =~ /(getch|nextc).$/ }.first
+      where = where.split(/:/).first(2).join(":")
+      if ENV["TALLY"] then
+        d getch:where
+      else
+        d getch:[c, where]
+      end
       c
     end
 
     alias :old_scan :scan
     def scan re
       s = old_scan re
-      where = caller[1].split(/:/).first(2).join(":")
-      d :scan => [s, where] if s
+      where = caller.drop_while { |s| s =~ /scan.$/ }.first
+      where = where.split(/:/).first(2).join(":")
+      if ENV["TALLY"] then
+        d scan:[where]
+      else
+        d scan:[s, where] if s
+      end
       s
     end
-  end
 
-  def d o
-    $stderr.puts o.inspect
+    def d o
+      STDERR.puts o.inspect
+    end
   end
 end
-
