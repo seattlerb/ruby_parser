dd0851f5674f5abfe517dacf738ff8efbff604fa
Martin Wilhelmi <mnin@mnin.de>
2022-01-11 12:53:09 +0100

    Add support for Ruby 3.1.0

--- lib/ruby_parser.rb
+++ lib/ruby_parser.rb
@@ -84,6 +84,7 @@ require "ruby30_parser"
 class RubyParser # HACK
   VERSIONS.clear # also a HACK caused by racc namespace issues
 
+  class V31 < ::Ruby31Parser; end
   class V30 < ::Ruby30Parser; end
   class V27 < ::Ruby27Parser; end
   class V26 < ::Ruby26Parser; end
